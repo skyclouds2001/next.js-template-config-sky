388775596e7119c6d8d5e51790590c5494a4e213
skyclouds2001 <skyclouds2001@163.com>
2023-01-16 16:42:53 +0800

    build: move out unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>next.js template</title>
+        <meta name="description" content="next.js template" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
