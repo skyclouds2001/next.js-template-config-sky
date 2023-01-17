00f590580b89212dc8f43038cc7c79529f5c7ba6
skyclouds2001 <skyclouds2001@163.com>
2023-01-17 10:20:50 +0800

    build: disable this ridiculous rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   extends: ['next/core-web-vitals', 'plugin:promise/recommended', 'plugin:n/recommended', 'plugin:jsdoc/recommended', 'plugin:prettier/recommended'],
   plugins: [],
-  rules: {},
+  rules: {
+    'n/no-missing-import': 'off',
+  },
   overrides: [],
 }
