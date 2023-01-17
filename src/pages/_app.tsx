4a8e0efbd1bab975ba472db1f5200441894bad9a
skyclouds2001 <skyclouds2001@163.com>
2023-01-17 12:28:48 +0800

    feat: update meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,43 @@ import '@/styles/globals.css'
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
+/**
+ *
+ * @param root0
+ * @param root0.Component
+ * @param root0.pageProps
+ */
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta name="application-name" content="next.js-template-sky" />
+        <meta name="author" content="skyclouds2001@163.com" />
+        <meta name="description" content="A template project for next.js by skyclouds2001." />
+        <meta name="generator" content="next.js" />
+        <meta name="keywords" content="next.js,template,config" />
+        <meta name="referrer" content="no-referer" />
+        <meta name="theme-color" media="(prefers-color-scheme:light)" content="white" />
+        <meta name="theme-color" media="(prefers-color-scheme:dark)" content="black" />
+        <meta name="color-schema" content="[light|dark]" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,viewport-fit=cover,user-scalable=yes" />
+        <meta name="creator" content="skyclouds2001@163.com" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="publisher" content="skyclouds2001@163.com" />
+        <meta name="robots" content="index,follow" />
+        <meta name="renderer" content="webkit" />
+        <meta name="force-rendering" content="webkit" />
+        <meta name="format-detection" content="telephone=no,date=no,address=no" />
+        <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+        <meta httpEquiv="content-type" content="text/html;charset=utf-8" />
+        <meta httpEquiv="content-security-policy" content="default-src 'self'" />
+        <meta property="og:title" content="next.js-template-sky" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="/" />
+        <meta property="og:image" content="/favicon.ico" />
+        <base href="/" target="_self" />
         <title>next.js template</title>
-        <meta name="description" content="next.js template" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
