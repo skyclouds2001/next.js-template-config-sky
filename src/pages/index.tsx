0292a168737f93e833aedceffb831c5c1d7c56ec
skyclouds2001 <skyclouds2001@163.com>
2023-01-20 00:25:26 +0800

    feat: use prop-types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import type { GetServerSideProps } from 'next'
 import { useSelector, useDispatch } from 'react-redux'
 import { increase, decrease } from '@/store'
 import type { RootState, AppDispatch } from '@/store'
 
-const Home: React.FC<{ name: string }> = (props) => {
+interface HomeProps {
+  name: string
+}
+
+const Home: React.FC<HomeProps> = (props) => {
   const value = useSelector<RootState, RootState['hello']['value']>((state) => state.hello.value)
   const dispatch = useDispatch<AppDispatch>()
 
@@ -21,7 +26,11 @@ const Home: React.FC<{ name: string }> = (props) => {
   )
 }
 
-export const getServerSideProps: GetServerSideProps<{ name: string }> = async () => {
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
   const res = await fetch('http://localhost:3000/api/hello')
   const data: { name: string } = await res.json()
   return { props: { name: data.name } }
