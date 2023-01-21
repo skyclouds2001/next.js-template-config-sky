ee84f435d742647710c84e23865191db87ac43c9
skyclouds2001 <skyclouds2001@163.com>
2023-01-21 13:13:19 +0800

    build: update test with mock

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,11 +1,23 @@
-import { describe, it, expect } from '@jest/globals'
+import { describe, it, expect, beforeAll, afterEach, afterAll } from '@jest/globals'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 import Home from '@/pages/index'
 import wrapper from './utils/render'
 
+const server = setupServer(
+  rest.get('/api/hello', (req, res, ctx) => {
+    return res(ctx.json({ name: 'sky' }))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
 describe('Home', () => {
-  it('renders a heading', () => {
+  it('renders a heading', async () => {
     render(<Home name="test" />, { wrapper })
 
     const heading = screen.getByRole('heading', {
